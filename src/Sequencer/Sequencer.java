a66741f89ac91f39987e8bb79a59454621c4d9d0
dgautam1604 <36433627+dgautam1604@users.noreply.github.com>
2020-04-10 14:32:04 -0400

    Update Sequencer.java
    Removed unwanted code

--- src/Sequencer/Sequencer.java
+++ src/Sequencer/Sequencer.java
@@ -1,4 +1,4 @@
-package Sequencer;
+package sequencer;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -7,53 +7,49 @@ import java.net.InetAddress;
 import java.net.SocketException;
 
 public class Sequencer {
-    private static int sequencerId = 1;
+	private static int sequencerId = 1;
+	public static void main(String[] args) {
+		DatagramSocket aSocket = null;
+		try {
+			aSocket = new DatagramSocket(1333);
+			byte[] buffer = new byte[1000];
+			System.out.println("Sequencer UDP Server Started");
+			while (true) {
+				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+				aSocket.receive(request);
+				String sentence = new String( request.getData(), 0,request.getLength() );
+				
+				//String[] parts = sentence.split(";");
+				System.out.println(sentence);
+				sendMessage(sentence);
+			}
 
-    public static void main(String[] args) {
-        DatagramSocket aSocket = null;
-        try {
-            aSocket = new DatagramSocket(1333);
-            byte[] buffer = new byte[1000];
-            System.out.println("Sequencer UDP Server 1313 Started............");
-            while (true) {
-                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
-                aSocket.receive(request);
-                String sentence = new String(request.getData(), 0,
-                        request.getLength());
+		} catch (SocketException e) {
+			System.out.println("Socket: " + e.getMessage());
+		} catch (IOException e) {
+			System.out.println("IO: " + e.getMessage());
+		} finally {
+			if (aSocket != null)
+				aSocket.close();
+		}
+	}
+	
+	public static void sendMessage(String message) {
+		int port=1412;
+		message = message+sequencerId;
+		sequencerId++;
+		
+		DatagramSocket aSocket = null;
+		try {
+			aSocket = new DatagramSocket();
+			byte[] messages = message.getBytes();
+			InetAddress aHost = InetAddress.getByName("230.1.1.10");
 
-                String[] parts = sentence.split(";");
-                String userID = parts[1];
-                System.out.println(sentence);
-                sendMessage(sentence);
-            }
+			DatagramPacket request = new DatagramPacket(messages, messages.length, aHost, port);
+			aSocket.send(request);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        } catch (SocketException e) {
-            System.out.println("Socket: " + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("IO: " + e.getMessage());
-        } finally {
-            if (aSocket != null)
-                aSocket.close();
-        }
-    }
-
-    public static void sendMessage(String message) {
-        int port = 1412;
-        message = message + sequencerId;
-        port = 1412;
-        sequencerId++;
-
-        DatagramSocket aSocket = null;
-        try {
-            aSocket = new DatagramSocket();
-            byte[] messages = message.getBytes();
-            InetAddress aHost = InetAddress.getByName("230.1.1.10");
-
-            DatagramPacket request = new DatagramPacket(messages, messages.length, aHost, port);
-            aSocket.send(request);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
+	}
 }
